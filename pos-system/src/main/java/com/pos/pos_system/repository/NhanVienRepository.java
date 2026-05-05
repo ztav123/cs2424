87c037e6b25ee4508c09e737749aee8d9219d06f
ztav123 <vinhdientrai2006@gmail.com>
2026-05-05 19:40:23 +0700

    Delete pos-system directory

--- pos-system/src/main/java/com/pos/pos_system/repository/NhanVienRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pos.pos_system.repository;
-
-import com.pos.pos_system.entity.NhanVien;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NhanVienRepository extends JpaRepository<NhanVien, Integer> {
-}
